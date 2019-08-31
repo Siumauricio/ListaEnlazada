f802f3c6889e762f3bdab5413f68afd544d64a18
Carlos Siu <47042324+Siumauricio@users.noreply.github.com>
2019-08-30 23:49:37 -0600

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "ListaOrdenada.h"
+
+int main() {
+	ListaOrdenada n1;
+	n1.agregar(26);
+	n1.agregar(24);
+	n1.agregar(65);
+	n1.agregar(89);
+	n1.imprimir();
+
+
+	return 0;
+}
